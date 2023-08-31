26991c1f773a1ffa25579ad99556074200895c5c
Randa Fathy Yahia AbdUllah <Randa.Yahia@andalusiagroup.net>
2023-08-31 17:01:05 +0300

    capture patient phase 2

--- tests/shared/components/capture-patient/capture-patient.spec.ts
+++ tests/shared/components/capture-patient/capture-patient.spec.ts
@@ -13,8 +13,17 @@ export const capturePatient = test('Capture Patient', async ({ page, context }) 
   await page.getByPlaceholder('Search For Patient (Name, Code, Phone Number)').press('Enter');
   await page.locator('.col-md-6').first().click();
   await page.locator('.col-md-4').click();
-  await page.getByRole('link').nth(1).click();
-  await page.getByRole('link', { name: 'portal Portal' }).click();
+  //navigate to portal
+  await page.locator('.start-nav').click()
+    const partialSrc = '/nav/Portal.svg'; // Partial match of the image src
+  //const linkSelector = `a:has(img[src*="${partialSrc}"])`;
+
+  const linkElement = await page.$(`a:has(img[src*="${partialSrc}"])`);
+  await expect(linkElement).toBeTruthy();
+  await linkElement.click()
+  
+  // await page.getByRole('link').nth(1).click();
+  // await page.getByRole('link', { name: 'portal Portal' }).click();
   await expect(page).toHaveURL(/.*physicianDesktop/);
   await page.getByRole('button', { name: 'Capture Patient' }).click();
   // Expect a title "to contain" a substring.
